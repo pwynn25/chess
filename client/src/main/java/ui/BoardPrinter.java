13c3558e315ec17bcbb92872daf6daa4db69cb77
Patrick Wynn <pw264@byu.edu>
2025-03-26 21:11:29 -0600

    printing white and black correctly

--- client/src/main/java/ui/BoardPrinter.java
+++ client/src/main/java/ui/BoardPrinter.java
@@ -11,36 +11,56 @@ public class BoardPrinter {
     private final int[] BLACK={1,9,8,0};
     public String printBoard(ChessBoard board, ChessGame.TeamColor teamColor) {
         int[] loopParams;
-        if(teamColor == ChessGame.TeamColor.WHITE) {
-            loopParams = WHITE;
-        }
-        else {
-            loopParams = BLACK;
-        }
         StringBuilder sb = new StringBuilder(SET_TEXT_COLOR_BLACK);
         String bgColor;
-        for (int i = loopParams[0]; i > loopParams[1]; i--) {
-            for (int j = loopParams[2]; j < loopParams[3]; j++) {
-                if ((i + j) % 2 == 0) {
-                    // Light square
-                    bgColor = SET_BG_COLOR_MAGENTA; // You can adjust this color for light squares
-                } else {
-                    // Dark square
-                    bgColor = SET_BG_COLOR_LIGHT_GREY; // You can set this to a color of your choice for dark squares
+        if(teamColor == ChessGame.TeamColor.WHITE) {
+            loopParams = WHITE;
+            for (int i = loopParams[0]; i > loopParams[1]; i--) {
+                for (int j = loopParams[2]; j < loopParams[3]; j++) {
+                    if ((i + j) % 2 == 0) {
+                        // Light square
+                        bgColor = SET_BG_COLOR_MAGENTA; // You can adjust this color for light squares
+                    } else {
+                        // Dark square
+                        bgColor = SET_BG_COLOR_LIGHT_GREY; // You can set this to a color of your choice for dark squares
+                    }
+                    sb.append(drawSpot(i,j,bgColor,board));
                 }
-                sb.append(bgColor).append(" ");
-                ChessPosition pos = new ChessPosition(i,j);
-                if(board.getPiece(pos) == null) {
-                    sb.append(" ");
-                }
-                else {
-                    sb.append(board.getPiece(new ChessPosition(i, j)));
-                }
-                sb.append(" ");
-                sb.append(RESET_BG_COLOR);
+                sb.append("\n");
             }
             sb.append("\n");
         }
+        else {
+            loopParams = BLACK;
+            for (int i = loopParams[0]; i < loopParams[1]; i++) {
+                for (int j = loopParams[2]; j > loopParams[3]; j--) {
+                    if ((i + j) % 2 == 0) {
+                    // Light square
+                        bgColor = SET_BG_COLOR_MAGENTA; // You can adjust this color for light squares
+                    } else {
+                    // Dark square
+                        bgColor = SET_BG_COLOR_LIGHT_GREY; // You can set this to a color of your choice for dark squares
+                    }
+                    sb.append(drawSpot(i, j, bgColor, board));
+                }
+                sb.append("\n");
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+
+    public String drawSpot(int i, int j,String bgColor, ChessBoard board) {
+        StringBuilder sb = new StringBuilder(SET_TEXT_COLOR_BLACK);
+        sb.append(bgColor);
+        ChessPosition pos = new ChessPosition(i,j);
+        if(board.getPiece(pos) == null) {
+            sb.append("   ");
+        }
+        else {
+            sb.append(" ").append(board.getPiece(new ChessPosition(i, j))).append(" ");
+        }
+        sb.append(RESET_BG_COLOR);
         return sb.toString();
     }
 
